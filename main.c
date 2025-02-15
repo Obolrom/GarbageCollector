9b0fa81f8ba037f1dd99ac1a1b7747ad89176c9b
Obolrom <obolrom@gmail.com>
2025-02-15 19:25:02 +0200

    Expand basic test case

--- main.c
+++ main.c
@@ -9,14 +9,28 @@ typedef struct CustomType {
     short test3;
 } Data;
 
+typedef struct CustomType2 {
+    int orderNum;
+    Data* data;
+} Data2;
+
+void test1(VM* vm);
+
 int main() {
     VM* vm = createVirtualMachine();
 
+    test1(vm);
+
+    return 0;
+}
+
+void test1(VM* vm) {
     int number = 500;
     float pi = 3.14f;
     short littleNum = 120;
     char test[] = "test data :)";
     Data data = { 255, 0xdeadbeaf, 18 };
+    Data2 data2 = { 1, &data };
 
     HeapObj* obj1 = createObject(vm, sizeof(number), &number);
     HeapObj* obj2 = createObject(vm, sizeof(pi), &pi);
@@ -24,6 +38,9 @@ int main() {
     HeapObj* obj4 = createObject(vm, sizeof(test), &test);
     HeapObj* obj5 = createObject(vm, sizeof(data), &data);
     HeapObj* obj6 = createObject(vm, sizeof(data), &data);
+    data.test1 += 245;
+    HeapObj* obj7 = createObject(vm, sizeof(data), &data);
+    HeapObj* obj8 = createObject(vm, sizeof(data2), &data2);
 
     printf("obj1: %p\n", (void*)obj1->data);
     printf("obj1: %d\n", *(int*)(obj1->data));
@@ -45,7 +62,19 @@ int main() {
     printf("obj6: %p\n", (void*)obj6->data);
     printf("obj6: %d\n", ((Data*)(obj6->data))->test1);
     printf("obj6: %lx\n", ((Data*)(obj6->data))->test2);
-    printf("obj6: %d\n", ((Data*)(obj6->data))->test3);
+    printf("obj6: %d\n\n", ((Data*)(obj6->data))->test3);
 
-    return 0;
+    printf("obj7: %p\n", (void*)obj7->data);
+    printf("obj7: %d\n", ((Data*)(obj7->data))->test1);
+    printf("obj7: %lx\n", ((Data*)(obj7->data))->test2);
+    printf("obj7: %d\n\n", ((Data*)(obj7->data))->test3);
+
+    printf("obj8: %p\n", (void*)obj8->data);
+    printf("obj8: %d\n", ((Data2*)(obj8->data))->orderNum);
+    printf("obj8: %d\n", ((Data*)((Data2*)(obj8->data))->data)->test1);
+    printf("obj8: %d\n", ((Data*)((Data2*)(obj8->data))->data)->test3);
+    data.test1 += 1000;
+    data.test3 += 82;
+    printf("obj8: %d\n", ((Data*)((Data2*)(obj8->data))->data)->test1);
+    printf("obj8: %d\n", ((Data*)((Data2*)(obj8->data))->data)->test3);
 }
