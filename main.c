314aff30a195a979ca90c91261e690d514762124
Obolrom <obolrom@gmail.com>
2025-12-25 22:42:07 +0200

    fix: move test case 11 from main.c to test/test_vm.c

--- main.c
+++ main.c
@@ -63,7 +63,6 @@ typedef struct CustomType4 {
 void test1(VM* vm);
 
 void testSuite();
-int testCase11();
 int testCase12();
 int testCase13();
 int testCase14();
@@ -98,7 +97,6 @@ int main() {
 }
 
 void testSuite() {
-    testCase11();
     testCase12();
     testCase13();
     testCase14();
@@ -113,75 +111,6 @@ void testSuite() {
     testCase23();
     testCase24();
     testCase25();
-}
-
-int testCase11_helperFunction_passed = 1;
-void testCase11_helperFunction(int32_t instructionPointer, int32_t stackTopValue) {
-    if (instructionPointer == 2 && stackTopValue != 10) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 4 && stackTopValue != 40) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 5 && stackTopValue != 50) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 6) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 7) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 8) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 9) {
-        testCase11_helperFunction_passed = 0;
-    }
-    if (instructionPointer == 10 && stackTopValue != 50) {
-        testCase11_helperFunction_passed = 0;
-    }
-}
-
-int testCase11() {
-    int passed = 1;
-    VM* vm = createVirtualMachine(240, 48);
-
-    int32_t bytecode[] = {
-            OP_PUSH, 10,
-            OP_PUSH, 40,
-            OP_ADD,
-            OP_JMP, 10,
-            OP_PUSH, 50,
-            OP_ADD,
-            OP_PRINT,
-            OP_HALT,
-    };
-
-    executeBytecode(vm, bytecode, NULL, testCase11_helperFunction);
-
-    passed = testCase11_helperFunction_passed;
-    for (int i = 0; i < OPERATION_STACK_SIZE; ++i) {
-        if (vm->stack[i] != -1) {
-            passed = 0;
-        }
-    }
-    if (vm->stackPointer != -1) {
-        passed = 0;
-    }
-
-#ifdef TEST_OUTPUT_ENABLED
-    if (passed == 1) {
-        printf(GREEN "Test 'testCase11' passed\n" RESET);
-    }
-    else {
-        printf(RED "Test 'testCase11' FAILED\n" RESET);
-    }
-#endif
-
-    destroyVirtualMachine(vm);
-
-    return passed;
 }
 
 int testCase12_helperFunction_passed = 1;
