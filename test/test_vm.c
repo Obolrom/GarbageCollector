09dc435bf2cbef2b5b1de1fb0cbfdb2d0dcc2ece
Obolrom <obolrom@gmail.com>
2025-12-28 00:38:31 +0200

    fix: move test case 21-25 from main.c to test/test_vm.c

--- test/test_vm.c
+++ test/test_vm.c
@@ -26,14 +26,14 @@ void test_1() {
         "Expected heap memory pointer is equal to obj1 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
             BI_RED,
             vm->heap->blocks[i]->busyIndicator,
             "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
             BI_GREEN,
             vm->heap->blocks[i]->busyIndicator,
@@ -76,14 +76,14 @@ void test_2() {
             "Expected obj2 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -126,14 +126,14 @@ void test_3() {
             "Expected obj2 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -209,14 +209,14 @@ void test_4() {
             "Expected obj2 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -300,14 +300,14 @@ void test_5() {
             "Expected obj4 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -335,7 +335,7 @@ void test_5() {
             vm->heap->blocks[2]->busyIndicator == BI_GREEN,
             "Expected heap.blocks[2] should be BI_GREEN"
     );
-    for (int i = 3; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 3; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
@@ -425,14 +425,14 @@ void test_6() {
             "Expected obj4 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -464,7 +464,7 @@ void test_6() {
             vm->heap->blocks[2]->busyIndicator == BI_GREEN,
             "Expected heap.blocks[2] should be BI_GREEN"
     );
-    for (int i = 3; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 3; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
@@ -554,14 +554,14 @@ void test_7() {
             "Expected obj4 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -586,7 +586,7 @@ void test_7() {
             getOccupiedHeapBlocksAmount(vm->heap) == 6,
             "Expected occupiedMemoryBlocks = 6"
     );
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         if (i == 3 || i == 4 || i == 5 || i == 12 || i == 13 || i == 14) {
             TEST_ASSERT_EQUAL_INT_MESSAGE(
                     BI_RED,
@@ -604,7 +604,7 @@ void test_7() {
     }
 
     findFreeBlockAddress(vm, 32);
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         if (i == 0 || i == 1 || i == 3 || i == 4 || i == 5 || i == 12 || i == 13 || i == 14) {
             TEST_ASSERT_EQUAL_INT_MESSAGE(
                     BI_RED,
@@ -621,7 +621,7 @@ void test_7() {
         }
     }
     findFreeBlockAddress(vm, 10);
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 12 || i == 13 || i == 14) {
             TEST_ASSERT_EQUAL_INT_MESSAGE(
                     BI_RED,
@@ -639,7 +639,7 @@ void test_7() {
     }
 
     findFreeBlockAddress(vm, 120);
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 12 || i == 13 || i == 14 ||
             i == 15 || i == 16 || i == 17 || i == 18 || i == 19 || i == 20 || i == 21 || i == 22) {
             TEST_ASSERT_EQUAL_INT_MESSAGE(
@@ -658,7 +658,7 @@ void test_7() {
     }
 
     findFreeBlockAddress(vm, 40);
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 12 || i == 13 || i == 14 ||
             i == 6 || i == 7 || i == 8 ||
             i == 15 || i == 16 || i == 17 || i == 18 || i == 19 || i == 20 || i == 21 || i == 22) {
@@ -775,14 +775,14 @@ void test_8() {
             "Expected obj4 pointer"
     );
 
-    for (int i = 0; i < occupiedMemoryBlocks; ++i) {
+    for (uint32_t i = 0; i < occupiedMemoryBlocks; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_RED,
                 vm->heap->blocks[i]->busyIndicator,
                 "Expected free heap blocks to be BI_RED"
         );
     }
-    for (int i = (int) occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = occupiedMemoryBlocks; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -823,7 +823,7 @@ void test_8() {
             "Expected occupiedMemoryBlocks = 15"
     );
 
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 12 || i == 13 || i == 14 ||
             i == 15 || i == 16 || i == 17 || i == 18 || i == 19 || i == 20) {
             TEST_ASSERT_EQUAL_INT_MESSAGE(
@@ -862,7 +862,7 @@ void test_8() {
             "Expected returnCode should be VM_RC_ERROR_OBJ_ALREADY_DELETED"
     );
 
-    for (int i = 0; i < vm->heap->blockAmount; ++i) {
+    for (uint32_t i = 0; i < vm->heap->blockAmount; ++i) {
         TEST_ASSERT_EQUAL_INT_MESSAGE(
                 BI_GREEN,
                 vm->heap->blocks[i]->busyIndicator,
@@ -1575,6 +1575,344 @@ void test_20() {
     destroyVirtualMachine(vm);
 }
 
+void test_21() {
+    VM* vm = createVirtualMachine(240, 48);
+    VmDebug *vmDebug = calloc(1, sizeof(VmDebug));
+    vmDebug->ipCount = 3;
+    vmDebug->pointers = malloc(sizeof(int32_t) * vmDebug->ipCount);
+    vmDebug->output = calloc(vmDebug->ipCount, sizeof(VmValue*));
+    vmDebug->pointers[0] = 2;
+    vmDebug->pointers[1] = 4;
+    vmDebug->pointers[2] = 5;
+
+    // should print 0 in stdout if VM_INTERPRETER_STDOUT_ENABLED
+    int32_t bytecode[] = {
+            OP_PUSH, 5,
+            OP_PUSH, 5,
+            OP_CMP_NEQ,
+            OP_PRINT,
+            OP_HALT,
+    };
+
+    executeBytecode(vm, bytecode, vmDebug, NULL);
+
+    for (int i = 0; i < OPERATION_STACK_SIZE; ++i) {
+        TEST_ASSERT_TRUE_MESSAGE(
+                vm->stack[i] == -1,
+                "Expected to be -1"
+        );
+    }
+    TEST_ASSERT_TRUE_MESSAGE(
+            vm->stackPointer == -1,
+            "Expected stackPointer to be -1"
+    );
+
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[0]->type == TYPE_INT && vmDebug->output[0]->intVal == 5,
+            "Expected output should be 5"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[1]->type == TYPE_INT && vmDebug->output[1]->intVal == 5,
+            "Expected output should be 5"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[2]->type == TYPE_INT && vmDebug->output[2]->intVal == 0,
+            "Expected output should be 0"
+    );
+
+    destroyVmDebug(vmDebug);
+    destroyVirtualMachine(vm);
+}
+
+void test_22() {
+    VM* vm = createVirtualMachine(240, 48);
+    VmDebug *vmDebug = calloc(1, sizeof(VmDebug));
+    vmDebug->ipCount = 3;
+    vmDebug->pointers = malloc(sizeof(int32_t) * vmDebug->ipCount);
+    vmDebug->output = calloc(vmDebug->ipCount, sizeof(VmValue*));
+    vmDebug->pointers[0] = 2;
+    vmDebug->pointers[1] = 8;
+    vmDebug->pointers[2] = 19;
+
+    // should print 15 in stdout if VM_INTERPRETER_STDOUT_ENABLED
+    int32_t bytecode[] = {
+            OP_PUSH, 5,
+            OP_PUSH, 10,
+            OP_CALL, 10, 2, 0,
+            OP_PRINT,
+            OP_HALT,
+
+            // function
+            OP_STORE, 0,
+            OP_STORE, 1,
+            OP_LOAD, 0,
+            OP_LOAD, 1,
+            OP_ADD,
+            OP_RET
+    };
+
+    executeBytecode(vm, bytecode, vmDebug, NULL);
+
+    for (int i = 0; i < OPERATION_STACK_SIZE; ++i) {
+        TEST_ASSERT_TRUE_MESSAGE(
+                vm->stack[i] == -1,
+                "Expected to be -1"
+        );
+    }
+    TEST_ASSERT_TRUE_MESSAGE(
+            vm->stackPointer == -1,
+            "Expected stackPointer to be -1"
+    );
+    TEST_ASSERT_NOT_NULL_MESSAGE(
+            vm->callStack,
+            "Expected to be not null"
+    );
+    TEST_ASSERT_NULL_MESSAGE(
+            vm->callStack->prevFrame,
+            "Expected callStack.prevFrame to be null"
+    );
+
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[0]->type == TYPE_INT && vmDebug->output[0]->intVal == 5,
+            "Expected output should be 5"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[1]->type == TYPE_INT && vmDebug->output[1]->intVal == 15,
+            "Expected output should be 15"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[2]->type == TYPE_INT && vmDebug->output[2]->intVal == 15,
+            "Expected output should be 15"
+    );
+
+    destroyVmDebug(vmDebug);
+    destroyVirtualMachine(vm);
+}
+
+void test_23() {
+    VM* vm = createVirtualMachine(240, 48);
+    VmDebug *vmDebug = calloc(1, sizeof(VmDebug));
+    vmDebug->ipCount = 3;
+    vmDebug->pointers = malloc(sizeof(int32_t) * vmDebug->ipCount);
+    vmDebug->output = calloc(vmDebug->ipCount, sizeof(VmValue*));
+    vmDebug->pointers[0] = 8;
+    vmDebug->pointers[1] = 19;
+    vmDebug->pointers[2] = 30;
+
+    // should print 7 in stdout if VM_INTERPRETER_STDOUT_ENABLED
+    int32_t bytecode[] = {
+            OP_PUSH, 5,
+            OP_PUSH, 10,
+            OP_CALL, 10, 2, 0,
+            OP_PRINT,
+            OP_HALT,
+
+            // function 1
+            OP_STORE, 0,
+            OP_STORE, 1,
+            OP_LOAD, 0,
+            OP_LOAD, 1,
+            OP_ADD,
+            OP_CALL, 22, 1,
+            OP_RET,
+
+            // function 2
+            OP_STORE, 0,
+            OP_LOAD, 0,
+            OP_PUSH, 8,
+            OP_SUB,
+            OP_RET,
+    };
+
+    executeBytecode(vm, bytecode, vmDebug, NULL);
+
+    for (int i = 0; i < OPERATION_STACK_SIZE; ++i) {
+        TEST_ASSERT_TRUE_MESSAGE(
+                vm->stack[i] == -1,
+                "Expected to be -1"
+        );
+    }
+    TEST_ASSERT_TRUE_MESSAGE(
+            vm->stackPointer == -1,
+            "Expected stackPointer to be -1"
+    );
+    TEST_ASSERT_NOT_NULL_MESSAGE(
+            vm->callStack,
+            "Expected to be not null"
+    );
+    TEST_ASSERT_NULL_MESSAGE(
+            vm->callStack->prevFrame,
+            "Expected callStack.prevFrame to be null"
+    );
+
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[0]->type == TYPE_INT && vmDebug->output[0]->intVal == 7,
+            "Expected output should be 7"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[1]->type == TYPE_INT && vmDebug->output[1]->intVal == 15,
+            "Expected output should be 15"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[2]->type == TYPE_INT && vmDebug->output[2]->intVal == 7,
+            "Expected output should be 7"
+    );
+
+    destroyVmDebug(vmDebug);
+    destroyVirtualMachine(vm);
+}
+
+void test_24() {
+    VM* vm = createVirtualMachine(240, 48);
+    VmDebug *vmDebug = calloc(1, sizeof(VmDebug));
+    vmDebug->ipCount = 3;
+    vmDebug->pointers = malloc(sizeof(int32_t) * vmDebug->ipCount);
+    vmDebug->output = calloc(vmDebug->ipCount, sizeof(VmValue*));
+    vmDebug->pointers[0] = 8;
+    vmDebug->pointers[1] = 39;
+    vmDebug->pointers[2] = 42;
+
+    // should print 20 and 100 in stdout if VM_INTERPRETER_STDOUT_ENABLED
+    int32_t bytecode[] = {
+            OP_PUSH, 0, // sum
+            OP_PUSH, 0, // i
+            OP_CALL, 10, 0, 2,
+            OP_PRINT,
+            OP_HALT,
+
+            // function
+            OP_STORE, 0, // sum
+            OP_STORE, 1, // i
+            OP_LOAD, 1,
+            OP_PUSH, 20,
+            OP_CMP_LT,
+            OP_JZ, 37,   // jump to end of loop
+            OP_LOAD, 0,
+            OP_PUSH, 5,
+            OP_ADD,
+            OP_STORE, 0,
+            OP_LOAD, 1,
+            OP_PUSH, 1,
+            OP_ADD,
+            OP_STORE, 1,
+            OP_JMP, 14,
+            OP_LOAD, 1,
+            OP_PRINT,    // print `i`
+            OP_LOAD, 0,  // put sum to op. stack
+            OP_RET
+    };
+
+    executeBytecode(vm, bytecode, vmDebug, NULL);
+
+    for (int i = 0; i < OPERATION_STACK_SIZE; ++i) {
+        TEST_ASSERT_TRUE_MESSAGE(
+                vm->stack[i] == -1,
+                "Expected to be -1"
+        );
+    }
+    TEST_ASSERT_TRUE_MESSAGE(
+            vm->stackPointer == -1,
+            "Expected stackPointer to be -1"
+    );
+    TEST_ASSERT_NOT_NULL_MESSAGE(
+            vm->callStack,
+            "Expected to be not null"
+    );
+    TEST_ASSERT_NULL_MESSAGE(
+            vm->callStack->prevFrame,
+            "Expected callStack.prevFrame to be null"
+    );
+
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[0]->type == TYPE_INT && vmDebug->output[0]->intVal == 100,
+            "Expected output should be 100"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[1]->type == TYPE_INT && vmDebug->output[1]->intVal == 20,
+            "Expected output should be 20"
+    );
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[2]->type == TYPE_INT && vmDebug->output[2]->intVal == 100,
+            "Expected output should be 100"
+    );
+
+    destroyVmDebug(vmDebug);
+    destroyVirtualMachine(vm);
+}
+
+void test_25() {
+    VM* vm = createVirtualMachine(240, 48);
+    VmDebug *vmDebug = calloc(1, sizeof(VmDebug));
+    vmDebug->ipCount = 1;
+    vmDebug->pointers = malloc(sizeof(int32_t) * vmDebug->ipCount);
+    vmDebug->output = calloc(vmDebug->ipCount, sizeof(VmValue*));
+    vmDebug->pointers[0] = 6;
+
+    // should print 5040 in stdout if VM_INTERPRETER_STDOUT_ENABLED
+    /*
+        int factorial(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            return n * factorial(n - 1);
+        }
+     */
+    int32_t bytecode[] = {
+            OP_PUSH, 7,
+            OP_CALL, 8, 1, 0,
+            OP_PRINT,
+            OP_HALT,
+
+            // factorial function
+            OP_STORE, 0,
+            OP_LOAD, 0,
+            OP_PUSH, 1,
+            OP_CMP_EQ,
+            OP_JNZ, 30,
+
+            OP_LOAD, 0,
+            OP_PUSH, 1,
+            OP_SUB,
+            OP_CALL, 8, 1, 0, // recursion call
+            OP_LOAD, 0,
+            OP_MUL,
+            OP_RET,
+
+            // base case
+            OP_PUSH, 1,
+            OP_RET
+    };
+
+    executeBytecode(vm, bytecode, vmDebug, NULL);
+
+    for (int i = 0; i < OPERATION_STACK_SIZE; ++i) {
+        TEST_ASSERT_TRUE_MESSAGE(
+                vm->stack[i] == -1,
+                "Expected to be -1"
+        );
+    }
+    TEST_ASSERT_TRUE_MESSAGE(
+            vm->stackPointer == -1,
+            "Expected stackPointer to be -1"
+    );
+    TEST_ASSERT_NOT_NULL_MESSAGE(
+            vm->callStack,
+            "Expected to be not null"
+    );
+    TEST_ASSERT_NULL_MESSAGE(
+            vm->callStack->prevFrame,
+            "Expected callStack.prevFrame to be null"
+    );
+
+    TEST_ASSERT_TRUE_MESSAGE(
+            vmDebug->output[0]->type == TYPE_INT && vmDebug->output[0]->intVal == 5040,
+            "Expected output should be 5040"
+    );
+
+    destroyVmDebug(vmDebug);
+    destroyVirtualMachine(vm);
+}
+
 int main() {
     UNITY_BEGIN();
 
@@ -1598,6 +1936,11 @@ int main() {
     RUN_TEST(test_18);
     RUN_TEST(test_19);
     RUN_TEST(test_20);
+    RUN_TEST(test_21);
+    RUN_TEST(test_22);
+    RUN_TEST(test_23);
+    RUN_TEST(test_24);
+    RUN_TEST(test_25);
 
     return UNITY_END();
 }
